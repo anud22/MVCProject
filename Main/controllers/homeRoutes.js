a63017383d22c53114519e76310631bd8319a49a
Preston Vongvilay <xprestonxd@gmail.com>
2023-08-01 19:34:16 -0700

    added code to login handlebar

--- Main/controllers/homeRoutes.js
+++ Main/controllers/homeRoutes.js
@@ -2,6 +2,9 @@ const router = require('express').Router();
 router.get('/', async (req, res) => {
     res.render('homepage');
 })
+router.get('/login', async (req, res) => {
+    res.render('login');
+})
 
 
 
