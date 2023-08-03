0cf8a6f862d6d7e36d782f3186cd4614aa0c8b80
anuD <adilbagi22@gmail.com>
2023-08-03 16:05:56 -0700

    Added Contribution model/route/handlebar

--- Main/server.js
+++ Main/server.js
@@ -13,6 +13,10 @@ const helpers = require('./utils/helpers');
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 const sess = {
   secret: 'Super secret secret',
   cookie: {
@@ -28,15 +32,12 @@ const sess = {
   })
 };
 
+
 app.use(session(sess));
 
 // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
