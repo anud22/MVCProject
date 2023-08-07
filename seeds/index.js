de6d57e3a388562166ef41eb746386bb3d98d1bc
yul402 <Ying.li20@outlook.com>
2023-08-06 20:45:54 -0500

    Added login/signup feature

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,9 @@
 const sequelize = require('../config/connection');
+const seedUser = require('./user');
 const seedFund = require('./fundData');
 const seedFundCategories = require('./fundCategory');
 const seedContributions = require('./contribution');
+
 
 const seedAll = async () => {
     await sequelize.sync({ force: true });
---
+++ seeds/user.js
@@ -0,0 +1,14 @@
+const { User } = require('../models');
+
+const userData = [
+  {
+    email: 'test@test.com',
+    lastName: 'test',
+    firstName: 'li',
+    password: 'Mi1234567*'
+  },
+];
+
+const seedUser = () => User.bulkCreate(userData);
+
+module.exports = seedUser;
